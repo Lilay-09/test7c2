26aeb3228d45019a6dc75790c44eedb6de0639ba
Lylay-09 <pozzlay549@gmail.com>
2023-07-05 14:23:08 +0700

    test4

--- pages/_document.js
+++ pages/_document.js
@@ -23,14 +23,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          {/* <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-            crossOrigin="anonymous"
-          /> */}
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
---
+++ pages/_error.js
@@ -0,0 +1,15 @@
+const ErrorPage = ({ statusCode }) => {
+  return (
+    <div>
+      <h1>Error {statusCode}</h1>
+      <p>Something went wrong.</p>
+    </div>
+  );
+};
+
+ErrorPage.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 500;
+  return { statusCode };
+};
+
+export default ErrorPage;
