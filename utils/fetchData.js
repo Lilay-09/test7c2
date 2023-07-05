189bb2f3488db8401820e00c94ad448d4392312d
Lylay-09 <pozzlay549@gmail.com>
2023-07-05 14:18:46 +0700

    test4

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,5 +1,5 @@
 export const postData = async (url, body) => {
-  const res = await fetch(`${process.env.BASE_API_URL_MAIN1}/api/${url}`, {
+  const res = await fetch(`${process.env.BASE_API_URL_MAIN_1}/api/${url}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
