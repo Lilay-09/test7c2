f308ac566c9d50222fd15f71951d969384256025
Lylay-09 <pozzlay549@gmail.com>
2023-07-05 16:26:28 +0700

    fetchData

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["admin.7c-kh.com", "127.0.0.1", "localhost"],
-    // unoptimized: true,
+    unoptimized: true,
+  },
+  env: {
+    API_URL: "https://admin.7c-kh.com",
   },
 };
 
--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,5 +1,5 @@
 export const postData = async (url, body) => {
-  const res = await fetch(`https://admin.7c-kh.com/api/${url}`, {
+  const res = await fetch(`${process.env.API_URL}/api/${url}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
