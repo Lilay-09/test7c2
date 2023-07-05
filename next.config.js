f20984f862e236ada95ed9d6ca71dd806c38e0b6
Lylay-09 <pozzlay549@gmail.com>
2023-07-05 15:05:54 +0700

    test5

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["admin.7c-kh.com", "127.0.0.1", "localhost"],
-    unoptimized: true,
+    // unoptimized: true,
   },
   env: {
     BASE_API_URL1: process.env.BASE_API_URL_1,
